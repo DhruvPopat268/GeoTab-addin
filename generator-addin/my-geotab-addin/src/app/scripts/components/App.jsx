759d1f45c4ef3540611f12b1e67a1243c820a626
Dhruv popat <dhruvpopat268@gmail.com>
2025-05-20 18:48:45 +0530

    cheking

--- generator-addin/my-geotab-addin/src/app/scripts/components/App.jsx
+++ generator-addin/my-geotab-addin/src/app/scripts/components/App.jsx
@@ -7,7 +7,7 @@ import DriverDetail from './DriverDetail.jsx'
 import GeotabContext from '../contexts/Geotab';
 import Logger from '../utils/logger';
 import APITable from './APITable.jsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 const App = ({ geotabApi, geotabState, appName }) => {
