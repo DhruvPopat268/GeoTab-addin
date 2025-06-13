14f198f3d5efdb00e9182c22ac6b7133ee23ea4f
Dhruv popat <dhruvpopat268@gmail.com>
2025-06-13 13:28:48 +0530

    checking

--- generator-addin/my-geotab-addin/src/env.js
+++ generator-addin/my-geotab-addin/src/env.js
@@ -1,3 +1,3 @@
 // src/env.js
-// export const BASE_URL = 'https://geotab-addin-backend-hlji.onrender.com';
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://geotab-addin-backend-hlji.onrender.com';
+// export const BASE_URL = 'http://localhost:5000';
