625836d949acf244c4cdc8da9c5d8764a4e8d112
DhruvPopat268 <dhruvpopat268@gmail.com>
2025-12-05 20:01:19 +0530

    added new domain

--- generator-addin/my-geotab-addin/src/env.js
+++ generator-addin/my-geotab-addin/src/env.js
@@ -2,4 +2,4 @@
 // export const BASE_URL = 'http://localhost:5000';
 export const BASE_URL = 'https://backend.c4u-online.co.uk';
 // export const BASE_URL = 'https://mygeotabaddin-backend-4pp72v-a7ed49-168-231-114-239.traefik.me';
-export const API_URL = 'https://api-monitoring-and-purchasing-platform-df9e.onrender.com';
+export const API_URL = 'https://admin.c4u-online.co.uk';
